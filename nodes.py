8f0c250f3ee04c39ac5bb2606e9bed7a97d454c4
endman100 <endman246738@gmail.com>
2024-06-06 10:17:55 +0800

    Update nodes.py

--- nodes.py
+++ nodes.py
@@ -6,12 +6,11 @@ from PIL import Image
 import numpy as np
 import torch
 import hashlib
-
+conditioning_dir = os.path.join(folder_paths.models_dir, "conditioning")
+if not os.path.exists(conditioning_dir):
+    os.makedirs(conditioning_dir)
 
 class SaveConditioning:
-    def __init__(self):
-        self.output_dir = folder_paths.get_output_directory()
-
     @classmethod
     def INPUT_TYPES(s):
         return {"required": 
@@ -26,7 +25,7 @@ class SaveConditioning:
     def save_conditioning(self, conditionings): # conditionings : [[text, {"pooled_output"}]...]
         results = list()
         for (batch_number, conditioning) in enumerate(conditionings):
-            save_path = os.path.join(self.output_dir, f"{batch_number:05}_conditionings.bin")
+            save_path = os.path.join(conditioning_dir, f"{batch_number:05}_conditionings.bin")
             print(conditioning[0].shape, conditioning[1]["pooled_output"].shape, save_path)
             torch.save(conditioning, save_path)
 
@@ -35,8 +34,7 @@ class SaveConditioning:
 class LoadContditioning():
     @classmethod
     def INPUT_TYPES(s):
-        input_dir = folder_paths.get_input_directory()
-        files = [f for f in os.listdir(input_dir) if os.path.isfile(os.path.join(input_dir, f)) and f.endswith(".bin")]
+        files = [f for f in os.listdir(conditioning_dir) if os.path.isfile(os.path.join(conditioning_dir, f)) and f.endswith(".bin")]
         return {"required": {"conditioning": [sorted(files), ]}, }
 
     CATEGORY = "endman100"
@@ -44,25 +42,25 @@ class LoadContditioning():
     FUNCTION = "load_conditioning"
 
     def load_conditioning(self, conditioning):
-        conditioning_path = folder_paths.get_annotated_filepath(conditioning)
+        conditioning_path = os.path.join(conditioning_dir, conditioning)
         conditioning_list = torch.load(conditioning_path)
         conditioning_list[0] = conditioning_list[0].cpu()
         conditioning_list[1]["pooled_output"] = conditioning_list[1]["pooled_output"].cpu()
-        print(conditioning_list)
         return ([conditioning_list], )
 
     @classmethod
     def IS_CHANGED(s, conditioning):
-        image_path = folder_paths.get_annotated_filepath(conditioning)
+        conditioning_path = os.path.join(conditioning_dir, conditioning)
         m = hashlib.sha256()
-        with open(image_path, 'rb') as f:
+        with open(conditioning_path, 'rb') as f:
             m.update(f.read())
         return m.digest().hex()
 
     @classmethod
     def VALIDATE_INPUTS(s, conditioning):
-        if not folder_paths.exists_annotated_filepath(conditioning):
-            return "Invalid latent file: {}".format(conditioning)
+        conditioning_path = os.path.join(conditioning_dir, conditioning)
+        if not os.path.exists(conditioning_path):
+            return "Invalid conditioning file: {}".format(conditioning_path)
         return True
     
 
