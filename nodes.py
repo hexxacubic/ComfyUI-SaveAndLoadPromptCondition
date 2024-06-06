aa85da30035e45266fe0ee68d94f1a0491f24675
endman100 <endman246738@gmail.com>
2024-06-06 13:54:42 +0800

    Update nodes.py

--- nodes.py
+++ nodes.py
@@ -6,11 +6,18 @@ from PIL import Image
 import numpy as np
 import torch
 import hashlib
-conditioning_dir = os.path.join(folder_paths.models_dir, "conditioning")
-if not os.path.exists(conditioning_dir):
-    os.makedirs(conditioning_dir)
+
+# set the models directory
+if "conditionings" not in folder_paths.folder_names_and_paths:
+    current_paths = [os.path.join(folder_paths.models_dir, "conditionings")]
+else:
+    current_paths, _ = folder_paths.folder_names_and_paths["conditionings"]
+folder_paths.folder_names_and_paths["conditionings"] = (current_paths, ".bin")
 
 class SaveConditioning:
+    def __init__(self):
+        self.output_dir = folder_paths.get_output_directory()
+
     @classmethod
     def INPUT_TYPES(s):
         return {"required": 
@@ -25,7 +32,7 @@ class SaveConditioning:
     def save_conditioning(self, conditionings): # conditionings : [[text, {"pooled_output"}]...]
         results = list()
         for (batch_number, conditioning) in enumerate(conditionings):
-            save_path = os.path.join(conditioning_dir, f"{batch_number:05}_conditionings.bin")
+            save_path = os.path.join(self.output_dir, f"{batch_number:05}_conditionings.bin")
             print(conditioning[0].shape, conditioning[1]["pooled_output"].shape, save_path)
             torch.save(conditioning, save_path)
 
@@ -34,20 +41,15 @@ class SaveConditioning:
 class LoadContditioning():
     @classmethod
     def INPUT_TYPES(s):
-        files = []
-        for root, dirs, files in os.walk(conditioning_dir):
-            for file in files:
-                if file.endswith(".bin"):
-                    file_path = os.path.join(root, file)
-                    files.append(file_path)
-        return {"required": {"conditioning": [sorted(files), ]}, }
+        print(folder_paths.get_filename_list("conditionings"))
+        return {"required": { "conditionings": (folder_paths.get_filename_list("conditionings"), )}}
 
     CATEGORY = "endman100"
     RETURN_TYPES = ("CONDITIONING", )
     FUNCTION = "load_conditioning"
 
     def load_conditioning(self, conditioning):
-        conditioning_path = os.path.join(conditioning_dir, conditioning)
+        conditioning_path = folder_paths.get_full_path("ipadapter", conditioning)
         conditioning_list = torch.load(conditioning_path)
         conditioning_list[0] = conditioning_list[0].cpu()
         conditioning_list[1]["pooled_output"] = conditioning_list[1]["pooled_output"].cpu()
@@ -55,7 +57,7 @@ class LoadContditioning():
 
     @classmethod
     def IS_CHANGED(s, conditioning):
-        conditioning_path = os.path.join(conditioning_dir, conditioning)
+        conditioning_path = folder_paths.get_full_path("ipadapter", conditioning)
         m = hashlib.sha256()
         with open(conditioning_path, 'rb') as f:
             m.update(f.read())
@@ -63,7 +65,7 @@ class LoadContditioning():
 
     @classmethod
     def VALIDATE_INPUTS(s, conditioning):
-        conditioning_path = os.path.join(conditioning_dir, conditioning)
+        conditioning_path = folder_paths.get_full_path("ipadapter", conditioning)
         if not os.path.exists(conditioning_path):
             return "Invalid conditioning file: {}".format(conditioning_path)
         return True
